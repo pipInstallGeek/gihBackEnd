d0d896a60cb592e959309bbe72d0fe1f78fb88f9
Marouane <pip.install.geek@gmail>
2023-03-16 13:19:45 +0100

    .

--- src/main/java/ma/uiass/eia/pds/Model/Lit.java
+++ src/main/java/ma/uiass/eia/pds/Model/Lit.java
@@ -20,7 +20,7 @@ public class Lit implements Serializable {
 
     @ManyToOne
     @JoinColumn(name = "idEspace", referencedColumnName = "idEspace")
-    private Chambre chambre;
+    private Espace espace;
 
     @ManyToOne
     @JoinColumn(name = "idType", referencedColumnName = "idType") // id de type de lit
@@ -32,16 +32,44 @@ public class Lit implements Serializable {
     private Marque marque;
     @OneToMany(mappedBy = "lit")
     private List<Equipement> equipements;
+
     public EtatLit getEtatLit() {
         return etatLit;
     }
 
-    public void setEtatLit(EtatLit etatLit) {
+    public void setEtatLit(EtatLit etatLit ) {
         this.etatLit = etatLit;
     }
 
-    public Lit(EtatLit etatLit) {
+    public TypeLit getTypeLit() {
+        return typeLit;
+    }
+
+    public void setTypeLit(TypeLit typeLit) {
+        this.typeLit = typeLit;
+    }
+
+    public Marque getMarque() {
+        return marque;
+    }
+
+    public void setMarque(Marque marque) {
+        this.marque = marque;
+    }
+
+    public Espace getEspace() {
+        return espace;
+    }
+
+    public void setEspace(Espace espace) {
+        this.espace = espace;
+    }
+
+    public Lit(Marque marque, TypeLit typeLit, EtatLit etatLit, Espace espace) {
+        setMarque(marque);
+        setTypeLit(typeLit);
         setEtatLit(etatLit);
+        setEspace(espace);
     }
     public Lit(){
     }
